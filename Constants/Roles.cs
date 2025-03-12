bfd0458438754869410f1536fb6e55cb16e788c2
CDKhoa362 <CDKhoa362@users.noreply.github.com>
2025-03-12 11:35:09 +0700

    Seeding Admin & Roles

---
+++ Constants/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace Diary_App.Constants
+{
+    public static class Roles
+    {
+        public const string ADMIN = "Admin";
+        public const string USER = "User";
+    }
+}
---
+++ Data/RoleSeeder.cs
@@ -0,0 +1,23 @@
+﻿using Diary_App.Constants;
+using Microsoft.AspNetCore.Identity;
+
+namespace Diary_App.Data
+{
+    public static class RoleSeeder
+    {
+        public static async Task SeedRoleAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            if (!await roleManager.RoleExistsAsync(Roles.ADMIN))
+            {
+                await roleManager.CreateAsync(new IdentityRole(Roles.ADMIN));
+            }
+
+            if(! await roleManager.RoleExistsAsync(Roles.USER))
+            {
+                await roleManager.CreateAsync(new IdentityRole(Roles.USER));
+            }
+        }
+    }
+}
