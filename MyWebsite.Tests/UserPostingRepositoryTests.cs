6a9e4f650ef917bc56547f5f0c462f0492485951
CDKhoa362 <CDKhoa362@users.noreply.github.com>
2025-03-14 19:16:14 +0700

    Change git folder position

---
+++ MyWebsite.Tests/UserPostingRepositoryTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyWebsite.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.InMemory;
+namespace MyWebsite.Tests
+{
+    internal class UserPostingRepositoryTests
+    {
+        private readonly DbContextOptions<ApplicationDbContext> _options;
+
+        // Cấu hình DbContext với InMemoryDatabase
+        public UserPostingRepositoryTests()
+        {
+            _options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("UserPostingDb")
+                .Options;
+        }
+
+        // Tạo Instance mới của DbContext
+        private ApplicationDbContext CreateDbContext() => new ApplicationDbContext(_options);
+
+
+
+
+
+    }
+}
